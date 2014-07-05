29a7fc55105bd4039a80b048d6aa18fced35fa92
mh <mh@immerda.ch>
2014-07-05 13:21:32 +0200

    initial release of module

---
+++ manifests/init.pp
@@ -0,0 +1,30 @@
+# manage a tircd installation
+# on debian
+class tircd(
+  $port = 16667,
+) {
+  package{'tircd':
+    ensure => installed,
+  } -> file{'/etc/default/tircd':
+    content => "ENABLED=1
+export HTTPS_CA_DIR=/usr/share/ca-certificates/
+",
+    notify  => Service['tircd'],
+    owner   => root,
+    group   => 0,
+    mode    => '0644';
+  } -> file_line{
+    'enable_ssl_on_tircd':
+      path    => '/etc/tircd.cfg',
+      line    => 'use_ssl 1',
+      pattern => '^use_ssl';
+    'tircd_port':
+      path    => '/etc/tircd.cfg',
+      line    => "port ${port}"
+      pattern => '^port',
+  } ~> service{'tircd':
+    ensure => running,
+    enable => true,
+  }
+
+}
